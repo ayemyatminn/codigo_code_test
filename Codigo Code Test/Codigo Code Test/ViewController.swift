9d7aa2bc9d1d57c64109f095a950138ff431a076
Aye Myat Min <ayemyat.minn@mm-digital-solutions.com>
2022-09-03 12:43:55 +0630

    initial commit

---
+++ Codigo Code Test/Codigo Code Test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Codigo Code Test
+//
+//  Created by Aye Myat Minn on 03/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
